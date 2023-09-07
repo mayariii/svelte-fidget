e766de5490e9cd0e49e3853d3994b43dfc83714c
Maya Avendano <mayari.avendano@gmail.com>
2023-09-07 22:04:10 +0100

    install tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen w-screen">
+    <div class="flex flex-col items-center justify-center h-full">
+<h1>
+    svelte fidget
+</h1>
+    </div>
+
+</div>
