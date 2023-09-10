1ec7e01fead0316f43b5ddf96982c60256fdd1f0
Maya Avendano <mayari.avendano@gmail.com>
2023-09-10 20:27:41 +0100

    update main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,14 +6,14 @@
 	<div class="flex flex-col gap-8 items-center justify-center h-full text-white">
 		<h1 class="text-3xl">svelte fidget</h1>
 
-		<p class="text-violet-300/80 text-sm max-w-[30ch] text-center">
-			drag along the x-axis to spin manually or click the button for auto-spin ✨
+		<p class="text-violet-300/80 text-sm max-w-[35ch] text-center">
+			swipe along the x-axis to spin manually or click the button for auto (infinite) spin ✨
 		</p>
 
 		<Spinner />
 
 		<p class="text-gray-500 text-xs">
-			made by <a href="https://mayari.io" class="text-violet-500">mayari.io</a>
+			made by <a href="https://mayari.io" target="_blank" class="text-violet-500">mayari.io</a>
 		</p>
 	</div>
 </div>
